238ce74d597d247892ee9812a05e4ee1643963dc
Wookiee- <taggart_james@hotmail.com>
2026-02-04 17:26:24 -0800

    removed some thing

--- codemp/server/sv_client.cpp
+++ codemp/server/sv_client.cpp
@@ -36,7 +36,6 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 #include "server/sv_gameapi.h"
 
 static void SV_CloseDownload( client_t *cl );
-int SV_RateMsec(client_t *client, int messageSize);
 
 /*
 =================
