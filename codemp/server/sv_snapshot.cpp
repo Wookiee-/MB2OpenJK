cce7a46efae489c128fb16cb628ae24e4e9538f2
Wookiee- <taggart_james@hotmail.com>
2026-03-17 18:21:40 -0700

    another fix

--- codemp/server/sv_snapshot.cpp
+++ codemp/server/sv_snapshot.cpp
@@ -23,6 +23,7 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 #include "server.h"
 #include "qcommon/cm_public.h"
+#include "qcommon/cm_public.h"
 
 /*
 =============================================================================
